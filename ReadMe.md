254c114da48717f5db0535f072b78cf05951d067
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-19 15:53:55 -0800

    add some notes

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,3 @@
+Run: ts-node index.ts
+
 For each lecture I created a folder for code at a given time. Change the name to index.ts and execute to run
--- intro/hello-world-ts.ts
+++ intro/hello-world-ts.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-
+//this lecture we just run some code in ts
 axios.get(url).then(response => {
     console.log(response.data)
 })
