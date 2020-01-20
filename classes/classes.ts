187ec4c27e94204219adbe5664bc56edc58d62fc
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-20 05:30:22 -0800

    classes refresher

---
+++ classes/classes.ts
@@ -0,0 +1,14 @@
+
+//class name should be capitalized
+class Vehicle {
+    drive(): void{
+        console.log('chugga chugga')
+    }
+    honk():void{
+        console.log('beep')
+    }
+}
+
+const vehicle = new Vehicle();
+vehicle.drive()
+vehicle.honk()
