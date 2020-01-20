d7434c51aab66bcd0a08a4b10c47d164ab79f369
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-19 16:31:47 -0800

    various types in ts

---
+++ annotations/variables.ts
@@ -0,0 +1,33 @@
+
+//common
+let apples: number = 5;
+let speed: string = 'fast'
+let hasName: boolean = true;
+
+//same type and value
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//built in object
+let now: Date = new Date();
+
+//array
+let colors: string[] = ['red', 'green', 'blue']
+let myNumbers: number[] = [1,2,3]
+let truths: boolean[] = [true, true, false]
+
+//classes
+class Car {
+    
+}
+
+//upper C referring to object class Car
+//car is a variable of type Car
+let car: Car
+
+//object literal
+
+let point: {x:number; y: number} = {
+    x: 10,
+    y: 20
+};
