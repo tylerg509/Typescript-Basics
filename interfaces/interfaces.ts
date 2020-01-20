be98da5e0cfa52aa92e77d26142d0b2001e1d5a5
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-20 05:21:59 -0800

    more interface ex

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -32,8 +32,31 @@ const printVehicleInterface = (vehicle: Vehicle): void => {
     console.log(`Name: ${vehicle.name}`)
     console.log(`Year: ${vehicle.year}`)
     console.log(`Broken: ${vehicle.broken}`)
+    console.log(vehicle.summary())
 }
 
 printVehicle(oldCivic);
 printVehicleInterface(oldCivic)
 
+interface Reportable{
+    summary(): string;
+}
+
+//note that you could delete name year borken from the interface and typescript would work as long as you only use summary in your function/whatever is using the interface
+const printSummary = (item: Reportable): void => {
+    console.log(item.summary())
+
+}
+
+//create another obj that uses summary
+const drink2 = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string{
+        return `My drink has ${this.sugar} grams of sugar`
+    }
+}
+
+//note how we can use now use printSummary using drink since both implement summary
+printSummary(drink2)
