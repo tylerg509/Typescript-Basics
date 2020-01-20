2c2c760ca98564961922ecc8fdc7ca41892bc396
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-19 19:44:39 -0800

    function inference

--- annotations/variable-functions-objects.ts
+++ annotations/variable-functions-objects.ts
@@ -5,7 +5,13 @@
 //type inference for functiosn = TS tried to figure out what type of value a funciton will return only
 
 //have to add type annotation to a and b
-//have to tell TS what this function will return "": number" means it will return number
+//have to tell TS what this function will return ": number" means it will return number
 const add = (a: number, b: number): number => {
     return  a + b;
+}
+
+//its good to use the return type annotation because it will ensure you return a value
+//if no retur ntype annotation you could return nothing and type inference would infer that return type is void
+const subtract = (a: number, b: number): number => {
+    return a-b;
 }
