c6757c65471ed380b82889323b2a4305264440b7
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-20 06:10:44 -0800

    classes and super

--- classes/classes2.ts
+++ classes/classes2.ts
@@ -2,9 +2,7 @@
 //class name should be capitalized
 //public is default
 class Vehicle2 {
-    constructor(public color: string){
-
-    }
+    constructor(public color: string){}
 
     //protected can only be called by child class
     protected honk():void{
@@ -16,6 +14,10 @@ class Vehicle2 {
 //use extends to inherit from vehicle
 //base inheritance in TS!
 class Car2 extends Vehicle2 {   
+    //dont use access modifier for color because it would create a new field color instead of using the color in base class
+    constructor(public wheels: number, color: string){
+        super(color)
+    }
       
     //override drive method in vehicle = polymorphism!
     private drive(): void{
@@ -34,5 +36,5 @@ console.log(vehicle2.color)
 
 
 //when we create instance of car the consturctor in vechincle gets called so we must pass color
-const car2 = new Car2('red')
+const car2 = new Car2(4 ,'red')
 console.log(car2.color)
